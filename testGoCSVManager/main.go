fdab19c41832d6e36e2b3e49509efb6f05f19a38
仲金德 <bunnyfaint@me.com>
2017-01-22 14:24:06 +0800

    增加测试用例

--- testGoCSVManager/main.go
+++ testGoCSVManager/main.go
@@ -11,13 +11,19 @@ func main() {
 
 	list := new(GoCSVManager.List)
 	list.Load("../csv/params.csv")
+	list.Load("../csv/banwords.csv")
 
-	fmt.Println(list.Tables)
+	// fmt.Println(list.Tables)
 	v, f := list.GetValueByFiled("params", "name", "BattleConfiguration", "desc")
 	fmt.Println("v:", v, f)
 
 	vs, fs := list.GetValuesByFiled("params", "name", "BattleConfiguration", "desc")
 	fmt.Println("vs:", vs, fs)
 
+	n, nbool := list.GetFirstValueByN("banwords", 5)
+	fmt.Println("n:", n, nbool)
+
+	table, _ := list.GetTable("banwords")
+	fmt.Println("Ban Line:", table.Size)
 	fmt.Println("CSVManager End...")
 }
