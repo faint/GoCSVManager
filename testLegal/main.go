0c2f24fec4cb6b813d5e8fdaa9a6ce38669e9aa9
仲金德 <bunnyfaint@me.com>
2017-01-22 13:14:49 +0800

    合法性检测

---
+++ testLegal/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+
+	"../../GoCSVManager"
+)
+
+func main() {
+	fmt.Println("检测CSV读取合法性")
+	list := new(GoCSVManager.List)
+
+	dirList, e := ioutil.ReadDir("../csv/")
+	if e != nil {
+		fmt.Println("read dir error")
+		return
+	}
+
+	count := 0
+	for _, v := range dirList {
+		fmt.Println("加载：", v.Name())
+
+		e := list.Load("../csv/" + v.Name())
+		if e != nil {
+			fmt.Println(e)
+		}
+
+		csvName := strings.Split(v.Name(), ".")[0]
+		fmt.Println("csvName:", csvName)
+		table, result := list.GetTable(csvName)
+		if !result {
+			fmt.Println(csvName, ":", result)
+			count++
+		}
+
+		fmt.Println("测试Key：", table.Keys)
+
+	}
+	fmt.Println("检测完毕，共有", count, "文件读取失败！!")
+}
