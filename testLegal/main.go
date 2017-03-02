7a232ea6167319c126b790ada82f9f4c539573e3
仲金德 <bunnyfaint@me.com>
2017-03-02 19:42:32 +0800

    because the path sep changed

--- testLegal/main.go
+++ testLegal/main.go
@@ -9,7 +9,7 @@ func main() {
 	fmt.Println("检测CSV读取合法性")
 	list := new(gocsv.List)
 
-	n, err := list.LoadDir("./csv/")
+	n, err := list.LoadDir("./csv")
 	if err != nil {
 		fmt.Println(err.Error())
 	}
